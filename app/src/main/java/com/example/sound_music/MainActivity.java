71c6fa1c524f62edcf7d8f9638d1617a302768cf
yuta <ogutoogumogura@gmail.com>
2019-04-07 23:50:20 +0900

    add sinwave switch

--- app/src/main/java/com/example/sound_music/MainActivity.java
+++ app/src/main/java/com/example/sound_music/MainActivity.java
@@ -3,16 +3,84 @@ package com.example.sound_music;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
+
+import org.puredata.android.io.AudioParameters;
+import org.puredata.android.io.PdAudio;
+import org.puredata.android.utils.PdUiDispatcher;
+import org.puredata.core.PdBase;
+import org.puredata.core.utils.IoUtils;
+
+import java.io.File;
+import java.io.IOError;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.sound_music.MESSAGE";
+
+    private PdUiDispatcher dispatcher;
+
+    private void initPD() throws IOException{
+        int sampleRate = AudioParameters.suggestSampleRate();
+        PdAudio.initAudio(sampleRate, 0,2,8,true);
+
+        dispatcher = new PdUiDispatcher();
+        PdBase.setReceiver(dispatcher);
+    }
+
+    private void initGUI(){
+        Switch onOffSwitch = (Switch) findViewById(R.id.onOffSwitch);
+        onOffSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.i("onOffSwitch",String.valueOf(isChecked));
+                float val = (isChecked) ?  1.0f :0.0f;
+                PdBase.sendFloat("onOff",val);
+            }
+        });
+
+    }
+
+    private void loadPDPatch() throws IOException{
+        File dir = getFilesDir();
+        IoUtils.extractZipResource(getResources().openRawResource(R.raw.simplepatch),dir,true);
+        File pdPatch = new File(dir,"simplepatch.pd");
+        PdBase.openPatch(pdPatch.getAbsolutePath());
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try{
+            initPD();
+            loadPDPatch();
+        }catch(IOException e){
+            finish();
+
+        }
+
+
+        initGUI();
     }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        PdAudio.startAudio(this);
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        PdAudio.stopAudio();
+
+    }
+
     /** Send buttonをユーザーがタップした時に呼ばれる */
     public void sendMessage(View view) {
         // ボタンに反応してなんかする
